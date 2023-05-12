586d11146e498cc73f56d31909bef2cb5281d2c4
Evgeniy Zlobina <ZEA_6590@mail.ru>
2023-05-12 22:08:08 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int summ = 100;
-        int summ1 = 3_000;
+        int balans = 100;
+        int depozitAmout = 3_000;
         int itog;
         int bonus;
-
-        if (summ1 > 1000) {itog = summ + summ1 + summ1/100; bonus = summ1/100;}
-        else {itog = summ + summ1; bonus = 0;};
+        if (depozitAmout > 1000) {
+            bonus = depozitAmout / 100;
+        } else {
+            bonus = 0;
+        }
+        itog = balans + depozitAmout + depozitAmout / 100;
         System.out.printf("итоговый счет = " + itog);
         System.out.println(" количество бонусных рублей = " + bonus);
-
-
     }
 }
